16a042d47f0d53da4b06247cde2bc5be4610bfd3
Augusto Cesar Scarpin <scarpinac@gmail.com>
2025-05-02 18:50:54 -0300

    ajuste de DataSeeders e atualização de pacotes composer

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Caminhao;
+use App\Models\Cliente;
+use App\Models\Fornecedor;
+use App\Models\Motorista;
+use App\Models\Pedidos;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,10 +19,17 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
-        User::factory()->create([
+        $this->call(UserSeeder::class);
+        $this->call(CaminhaoSeeder::class);
+        $this->call(ClienteSeeder::class);
+        $this->call(FornecedorSeeder::class);
+        $this->call(MotoristaSeeder::class);
+        $this->call(ProdutoSeeder::class);
+        $this->call(PedidoSeeder::class);
+        $this->call(ProdutoFornecedorSeeder::class);
+        /*User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
-        ]);
+        ]);*/
     }
 }
